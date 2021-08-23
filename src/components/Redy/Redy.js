79b8984ddba7cf8b2a8d2be70d1b25b4c3c8a7dd
Najmul Alam Miraj <najmulalammiraj@gmail.com>
2021-08-24 02:45:44 +0600

    m

--- src/components/Redy/Redy.js
+++ src/components/Redy/Redy.js
@@ -14,7 +14,7 @@ const Redy = () => {
   } = useForm();
   const [loggedInUser, setLoggedInUser] = useContext(UserContext);
   const onSubmit = (data) => {
-    fetch('https://islavo.web.app/api/users/createPaymentLink', {
+    fetch('http://localhost:5000/api/users/createPaymentLink', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
